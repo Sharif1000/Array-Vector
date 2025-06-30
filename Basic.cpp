bd673afae5f525fa075d53836bd252557a3cdcef
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-06-30 11:54:17 +0600

    Create Basic.cpp

---
+++ Basic.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  vector<int>v(n);
+
+  // How to sort a vector as ascending order
+  sort(v.begin(), v.end()); // O(nlogn)
+
+  // How to sort a vector as decending order
+  sort(v.rbegin(), v.rend()); // O(nlogn)
+
+  // How to reverse the order of a vector
+  reverse(v.begin(), v.end()); // O(n)
+
+  
+  return 0;
+}
